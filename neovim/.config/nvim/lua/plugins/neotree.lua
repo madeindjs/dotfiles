d68ab0b9637e99552ecb057029627f64ba0af177
madeindjs <alexandre@rsseau.fr>
2024-10-29 13:00:53 +0100

    neovim: improve neotree grep

--- neovim/.config/nvim/lua/plugins/neotree.lua
+++ neovim/.config/nvim/lua/plugins/neotree.lua
@@ -1,30 +1,7 @@
-local function getTelescopeOpts(state, path)
-  return {
-    cwd = path,
-    search_dirs = { path },
-    attach_mappings = function(prompt_bufnr, map)
-      local actions = require("telescope.actions")
-      actions.select_default:replace(function()
-        actions.close(prompt_bufnr)
-        local action_state = require("telescope.actions.state")
-        local selection = action_state.get_selected_entry()
-        local filename = selection.filename
-        if filename == nil then
-          filename = selection[1]
-        end
-        -- any way to open the file without triggering auto-close event of neo-tree?
-        require("neo-tree.sources.filesystem").navigate(state, state.path, filename)
-      end)
-      return true
-    end,
-  }
-end
-
 return {
   {
-
     "nvim-neo-tree/neo-tree.nvim",
-    opts = function(_, opts)
+    opts = function()
       return {
         filesystem = {
           window = {
@@ -40,12 +17,18 @@ return {
           telescope_find = function(state)
             local node = state.tree:get_node()
             local path = node:get_id()
-            require("telescope.builtin").find_files(getTelescopeOpts(state, path))
+            require("telescope.builtin").find_files({
+              cwd = path,
+              search_dirs = { path },
+            })
           end,
           telescope_grep = function(state)
             local node = state.tree:get_node()
             local path = node:get_id()
-            require("telescope.builtin").live_grep(getTelescopeOpts(state, path))
+            require("telescope.builtin").live_grep({
+              cwd = path,
+              search_dirs = { path },
+            })
           end,
         },
       }
