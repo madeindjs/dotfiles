16fab25a69044b76b7d2f26ee314a3de9d612d4b
madeindjs <alexandre@rsseau.fr>
2024-11-04 12:05:09 +0100

    neovim: move neotree conf

--- neovim/.config/nvim/lua/plugins/overides.lua
+++ neovim/.config/nvim/lua/plugins/overides.lua
@@ -58,4 +58,43 @@ return {
       },
     },
   },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    dependencies = { "nvim-telescope/telescope.nvim" },
+    opts = function()
+      return {
+        window = {
+          position = "right", -- setup Tree to the right
+        },
+        filesystem = {
+          window = {
+            mappings = {
+              ["t"] = "noop",
+              -- add telescope shortcuts
+              ["tf"] = "telescope_find",
+              ["tg"] = "telescope_grep",
+            },
+          },
+        },
+        commands = {
+          telescope_find = function(state)
+            local node = state.tree:get_node()
+            local path = node:get_id()
+            require("telescope.builtin").find_files({
+              cwd = path,
+              search_dirs = { path },
+            })
+          end,
+          telescope_grep = function(state)
+            local node = state.tree:get_node()
+            local path = node:get_id()
+            require("telescope.builtin").live_grep({
+              cwd = path,
+              search_dirs = { path },
+            })
+          end,
+        },
+      }
+    end,
+  },
 }
