b17dd325c94844c00fbb2e518c9f493241298759
madeindjs <alexandre@rsseau.fr>
2024-11-12 17:40:47 +0100

    neovim: install Oil

--- neovim/.config/nvim/lua/plugins/custom.lua
+++ neovim/.config/nvim/lua/plugins/custom.lua
@@ -7,4 +7,25 @@ return {
     "lima1909/resty.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
   },
+  {
+    "stevearc/oil.nvim",
+    ---@module 'oil'
+    ---@type oil.SetupOpts
+    opts = {},
+    -- config = function()
+    --   local oil = require("oil")
+    --   oil.setup()
+    -- end,
+    keys = {
+      -- add a keymap to browse plugin files
+      -- stylua: ignore
+      {
+        "<leader>c-",
+        function ()
+          require('oil').toggle_float()
+        end,
+        desc = "Edit files",
+      },
+    },
+  },
 }
